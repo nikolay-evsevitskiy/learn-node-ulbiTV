2c3938a3f630344a5a9ae90ab3e79562e3d48028
Nikolay Evsevitski <nikolay.evsevitskiy@gmail.com>
2023-06-15 19:00:37 +0200

    create process.argv

--- lessons/process.js
+++ lessons/process.js
@@ -1,6 +1,17 @@
-console.log(process.pid);
+// const dotenv = require('dotenv')
+// dotenv.config()
+//
+//
+// console.log(process.pid);
+//
+// console.log(process.env.PORT);
+// console.log(process.env.NODE_ENV);
 
-console.log(process.env.PORT);
-console.log(process.env.NODE_ENV);
+console.log(process.argv);
 
+if (Math.random() > 0.5) {
 
+} else {
+    console.log('выполнение программы завершено!')
+    process.exit()
+}

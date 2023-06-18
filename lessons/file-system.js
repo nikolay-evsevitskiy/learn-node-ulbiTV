d0a5e3d1c21ee73cd3439960111ef8a08f543c34
Nikolay Evsevitski <nikolay.evsevitskiy@gmail.com>
2023-06-18 15:04:24 +0200

    async create write and append files

--- lessons/file-system.js
+++ lessons/file-system.js
@@ -21,16 +21,46 @@ const path = require('path');
 // })
 
 //Создание файла
-fs.writeFile(path.resolve(__dirname, 'test.txt'), '5 jweihfkfkkvfkfjh', (err) => {
-    if (err) {
-        throw err
-    }
-    console.log('Файл записан!')
-})
+// fs.writeFile(path.resolve(__dirname, 'test.txt'), '5 jweihfkfkkvfkfjh', (err) => {
+//     if (err) {
+//         throw err
+//     }
+//     console.log('Файл записан!')
+// })
+//
+// fs.appendFile(path.resolve(__dirname, 'test.txt'), '1111111111', (err) => {
+//     if (err) {
+//         throw err;
+//     }
+//     console.log('Файл записан!')
+// })
+const pathOfFile = path.resolve(__dirname, 'test.txt')
+const data1 = 'Hello World!'
+const data2 = 'Goodbye World!'
 
-fs.appendFile(path.resolve(__dirname, 'test.txt'), '1111111111', (err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Файл записан!')
-})
+const writeFileAsync = async (path, data) => {
+    return new Promise((resolve, reject) => fs.writeFile(path, data, (err) => {
+        if (err) {
+            reject(err.message)
+        }
+        resolve('Файл записан!')
+    }))
+}
+
+const appendFileAsync = async (path, data) => {
+    return new Promise((resolve, reject) => fs.appendFile(path, data, (err) => {
+        if (err) {
+            reject(err.message)
+        }
+        resolve('Файл записан!')
+    }))
+}
+
+
+writeFileAsync(pathOfFile, data1)
+    .then(() => appendFileAsync(pathOfFile, data2))
+    .then(() => appendFileAsync(pathOfFile, '123'))
+    .then(() => appendFileAsync(pathOfFile, '456'))
+    .then(() => appendFileAsync(pathOfFile, '789'))
+    .catch(err => console.log(err))
+

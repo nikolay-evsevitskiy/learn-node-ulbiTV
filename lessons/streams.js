383f45780d4ed74656f31498298d1d9134b77c11
Nikolay Evsevitski <nikolay.evsevitskiy@gmail.com>
2023-06-22 21:37:25 +0200

    work with stream write and pipe

--- lessons/streams.js
+++ lessons/streams.js
@@ -14,12 +14,35 @@ const path = require('path')
 //     }
 // })
 
-const stream = fs.createReadStream(path.resolve(__dirname, 'test.txt'))
+// const stream = fs.createReadStream(path.resolve(__dirname, 'test.txt'))
 //One chunk is weight 64kb in default
-stream.on('data', (chunk) => {
-    console.log(chunk)
-})
+// stream.on('data', (chunk) => {
+//     console.log(chunk)
+// })
 
-stream.on('end', () => console.log('Read end'))
-stream.on('open', () => console.log('Read start'))
-stream.on('error', (err) => console.log(err))
+// stream.on('end', () => console.log('Read end'))
+// stream.on('open', () => console.log('Read start'))
+// stream.on('error', (err) => console.log(err))
+
+// const writableStream = fs.createWriteStream(path.resolve(__dirname, 'test2.txt'))
+//
+// for (let i = 0; i < 20; i++) {
+//     writableStream.write(i + '\n');
+// }
+//
+// writableStream.end()
+// writableStream.close()
+// writableStream.destroy()
+// writableStream.on('error')
+
+
+const http = require('http');
+
+http.createServer((req, res) => {
+    //req - readable stream
+    //res - writable stream
+    const stream = fs.createReadStream(path.resolve(__dirname, 'test.txt'))
+
+    //Stream will finish to read earlier than customer  download
+    stream.pipe(res)
+})

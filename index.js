20425084e305fe7ba4030fd2833fc6952c2a8310
Nikolay Evsevitski <nikolay.evsevitskiy@gmail.com>
2023-06-23 17:51:04 +0200

    create framework for working with http

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const http = require('http');
+const EventEmitter = require('events');
 const PORT = process.env.PORT || 5000;
+
+const emitter = new EventEmitter()
+
+class Router {
+    constructor() {
+        this.endpoints = {}
+    }
+
+    request(method = 'GET', path, handler) {
+        if (!this.endpoints[path]) {
+            this.endpoints[path] = {}
+        }
+        const endpoint = this.endpoints[path];
+
+        if (endpoint[method]) {
+            throw Error(`${method} in this address ${path} exist yet`)
+        }
+        endpoint[method] = handler
+        emitter.on(`[${path}]:[${method}]`, (req, res) => {
+            handler(req, res)
+
+        })
+    }
+}
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {

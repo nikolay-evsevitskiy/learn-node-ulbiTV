875946da37b6bfc1ffb6467948fe25383b7402f4
Nikolay Evsevitski <nikolay.evsevitskiy@gmail.com>
2023-06-24 22:20:23 +0200

    create CRUD operations

--- index.js
+++ index.js
@@ -24,21 +24,39 @@ class Router {
 
         })
     }
+
+    get(path, handler) {
+        this.request('GET', path, handler)
+    }
+
+    post(path, handler) {
+        this.request('POST', path, handler)
+    }
+
+    put(path, handler) {
+        this.request('PUT', path, handler)
+    }
+
+    delete(path, handler) {
+        this.request('DELETE', path, handler)
+    }
 }
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-type': 'application/json'
-    })
-    if (req.url === '/users') {
-        return res.end(JSON.stringify([
-            {id: 1, name: 'Nikolay'}
-        ]))
-    }
-    if (req.url === '/posts') {
-        return res.end('POSTS')
-    }
+const router = new Router();
 
+router.get('/users', (req, res) => {
+    res.end('YOU SEND REQUEST TO /USERS')
+})
+
+router.get('/posts',  (req, res) => {
+    res.end('YOU SEND REQUEST TO /POSTS')
+})
+
+const server = http.createServer((req, res) => {
+    const emitted = emitter.emit(`[${req.url}]:[${req.method}]`, req, res)
+    if (!emitted) {
+        res.end()
+    }
 })
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`))

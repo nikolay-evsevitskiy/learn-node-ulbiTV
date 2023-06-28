d93186143f374049d6b247e7b32dc14d875e7f68
Nikolay Evsevitski <nikolay.evsevitskiy@gmail.com>
2023-06-28 19:13:03 +0200

    install mongoose and start work with mongodb

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const Application = require('./framework/Application');
 const userRouter = require('./src/user-router');
 const jsonParser = require('./framework/parseJson');
 const urlParser = require('./framework/parseUrl');
+const mongoose = require('mongoose');
 
 const application = new Application();
 
 application.use(jsonParser)
 application.use(urlParser('http://localhost:5000'))
 application.addRouter(userRouter);
-application.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+
+const start = async () => {
+    try {
+        await mongoose.connect()
+        application.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()

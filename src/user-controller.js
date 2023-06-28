77af198325b9cab95ac27da54cd9ffabb9c06610
Nikolay Evsevitski <nikolay.evsevitskiy@gmail.com>
2023-06-28 19:25:27 +0200

    change logic in user controller for working with api mongoose

--- src/user-controller.js
+++ src/user-controller.js
@@ -1,20 +1,21 @@
-const users = [
-    {id: 1, name: 'Nikolay'},
-    {id: 2, name: 'Alex'},
-]
+const User = require('./user-model');
 
-const getUser = (req, res) => {
-    if (req.params.id) {
-        return res.send(users.find(user => user.id == req.params.id))
+const getUser = async (req, res) => {
+    const id = req.params.id;
+    let users;
+    if (id) {
+        users = await User.findById(id)
+
+    } else {
+        users = await User.find()
     }
     res.send(users);
 }
 
-const createUser = (req, res) => {
-    console.log(req.body)
-    const user = req.body;
-    users.push(user)
-    res.send(users);
+const createUser = async (req, res) => {
+
+    const user = await User.create(req.body);
+    res.send(user);
 }
 
 module.exports = {

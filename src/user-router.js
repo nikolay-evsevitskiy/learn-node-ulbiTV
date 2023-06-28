e3ae80c819fdbf0b16d5b941026d9affbaed5aca
Nikolay Evsevitski <nikolay.evsevitskiy@gmail.com>
2023-06-28 15:06:19 +0200

    create user-controller

--- src/user-router.js
+++ src/user-router.js
@@ -1,24 +1,8 @@
 const Router = require('../framework/Router');
-
+const controller = require('./user-controller')
 const router = new Router()
 
-const users = [
-    {id: 1, name: 'Nikolay'},
-    {id: 2, name: 'Alex'},
-]
-
-router.get('/users', (req, res) => {
-    if (req.params.id) {
-        return res.send(users.find(user => user.id == req.params.id))
-    }
-    res.send(users);
-});
-
-router.post('/users', (req, res) => {
-    console.log(req.body)
-    const user = req.body;
-    users.push(user)
-    res.send(users);
-});
+router.get('/users', controller.getUser);
+router.post('/users', controller.createUser);
 
 module.exports = router
